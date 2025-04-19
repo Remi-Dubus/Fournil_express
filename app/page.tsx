8b4c4714b958a8b969e03f248a4f37cb562ff21d
REMI DUBUS <remdubus@gmail.com>
2025-04-19 13:14:09 +0200

    feat: add presentation page closes #4

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Presentation from "@/components/Presentation";
+
 export default function Home() {
 	return (
 		<main className="h-fit min-h-[85vh] p-2 sm:grid sm:grid-cols-2 sm:gap-2 lg:gap-4 lg:pt-10 2xl:px-60">
-			{" "}
+			<Presentation />
 		</main>
 	);
 }
