6988eeb58460d26111aee1be2e5c9a69da4907c3
REMI DUBUS <remdubus@gmail.com>
2025-05-06 10:05:13 +0200

    feat: bakery can see validate and pending orders closes #9

--- src/entities/bakery/order/readAll.ts
+++ src/entities/bakery/order/readAll.ts
@@ -8,17 +8,10 @@ const sql = postgres(process.env.POSTGRES_URL as string, { ssl: "require" });
 
 export async function readAllOrders(id_bakery: string) {
 	const result = await sql<ordersListType[]>`
-            SELECT b.id AS booking_id,
-                b.order_number,
-                b.date,
-                b.validate,
-                b.hidden_bakery,
-                r.label AS restaurant_name,
-                r.email AS restaurant_email,
-                p.label AS product_name,
-                p.price,
-                p.id AS product_id,
-                bp.quantity
+            SELECT b.id AS booking_id, b.order_number, b.date, b.validate, b.hidden_bakery,
+            r.label AS restaurant_name, r.email AS restaurant_email, p.label AS product_name,
+            p.price, p.id AS product_id, 
+            bp.quantity
             FROM booking AS b
             JOIN booking_product AS bp ON b.id = bp.id_booking
             JOIN product AS p ON bp.id_product = p.id
