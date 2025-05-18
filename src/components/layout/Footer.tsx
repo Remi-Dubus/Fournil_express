a37a7166b8b652a2d242e95c23bfe1f9db3b0c44
REMI DUBUS <remdubus@gmail.com>
2025-05-18 09:53:52 +0200

    fix: height of footer closes #69

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,9 +4,9 @@ import data from "../../assets/data/layout.json";
 
 export default function Footer() {
 	return (
-		<footer className="flex flex-col items-center justify-center bg-dark text-light w-full h-16 z-4 static md:h-24 xl:z-0">
-			<p className="xl:ml-80 xl:inline-block">&copy; {data.copyrigth}</p>
-			<p className="xl:ml-80 xl:inline-block">
+		<footer className="flex flex-col items-center justify-center bg-dark text-light w-full h-16 z-4 static sm:h-24 xl:z-0">
+			<p className="xl:inline-block">&copy; {data.copyrigth}</p>
+			<p className="xl:inline-block">
 				{data.pictureCopyrigth}
 				<Link className="text-interest xl:inline-block" href={data.pixabayLink}>
 					{data.pixabay}
